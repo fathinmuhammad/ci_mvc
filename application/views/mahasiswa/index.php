10d2b59ec3f5f930b52eac7ddc9d80386b0e1bf9
Fathin Muhammad <fathinmuhammad40@gmail.com>
2019-07-08 18:40:43 +0700

    test commit

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -1,16 +1,16 @@
 <div class="container">
     <div class="flash-data" data-flashdata="<?= $this->session->flashdata('flash'); ?>"></div>
     <?php if ($this->session->flashdata('flash')) : ?>
-    <!-- <div class="row mt-3">
-        <div class="col-md-6">
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                Data mahasiswa <strong>berhasil</strong> <?= $this->session->flashdata('flash'); ?>.
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+        <!-- <div class="row mt-3">
+            <div class="col-md-6">
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    Data mahasiswa <strong>berhasil</strong> <?= $this->session->flashdata('flash'); ?>.
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
             </div>
-        </div>
-    </div> -->
+        </div> -->
     <?php endif; ?>
 
     <div class="row mt-3">
@@ -38,20 +38,17 @@
             <h3>Daftar Mahasiswa</h3>
             <?php if (empty($mahasiswa)) : ?>
                 <div class="alert alert-danger" role="alert">
-                data mahasiswa tidak ditemukan.
+                    data mahasiswa tidak ditemukan.
                 </div>
             <?php endif; ?>
             <ul class="list-group">
                 <?php foreach ($mahasiswa as $mhs) : ?>
-                <li class="list-group-item">
-                    <?= $mhs['nama']; ?>
-                    <a href="<?= base_url(); ?>mahasiswa/hapus/<?= $mhs['id']; ?>"
-                        class="badge badge-danger float-right tombol-hapus">hapus</a>
-                    <a href="<?= base_url(); ?>mahasiswa/ubah/<?= $mhs['id']; ?>"
-                        class="badge badge-success float-right">ubah</a>
-                    <a href="<?= base_url(); ?>mahasiswa/detail/<?= $mhs['id']; ?>"
-                        class="badge badge-primary float-right">detail</a>
-                </li>
+                    <li class="list-group-item">
+                        <?= $mhs['nama']; ?>
+                        <a href="<?= base_url(); ?>mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge badge-danger float-right tombol-hapus">hapus</a>
+                        <a href="<?= base_url(); ?>mahasiswa/ubah/<?= $mhs['id']; ?>" class="badge badge-success float-right">ubah</a>
+                        <a href="<?= base_url(); ?>mahasiswa/detail/<?= $mhs['id']; ?>" class="badge badge-primary float-right">detail</a>
+                    </li>
                 <?php endforeach; ?>
             </ul>
         </div>
